57d383ca61df026d24705ad1c5e315b65c4ffbe4
Stephen J. Butler <stephen.butler@gmail.com>
2018-02-07 21:00:58 -0600

    Fixup some bugs with DynamoDB session init

--- pie-dynamodb-sessions/load.php
+++ pie-dynamodb-sessions/load.php
@@ -35,16 +35,24 @@
 
 # Load this file in order to enable PHP Session handler for DynamoDB
 
-require_once( dirname( __FILE__ ) . 'vendor/autoload.php' );
+require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
 
 function pie_dynamodb_register_session_handler() {
     global $pie_dynamodb_session_handler;
 
-    $dynamodb = Aws\DynamoDb\DynamoDbClient();
+    $region = empty( $_SERVER[ 'PIE_DYNAMODB_SESSION_TABLE_REGION' ] )
+        ? 'us-east-2'
+        : $_SERVER[ 'PIE_DYNAMODB_SESSION_TABLE_REGION' ];
+    $name = empty( $_SERVER[ 'PIE_DYNAMODB_SESSION_TABLE_NAME' ] )
+        ? 'sessions'
+        : $_SERVER[ 'PIE_DYNAMODB_SESSION_TABLE_NAME' ];
+
+    $dynamodb = new \Aws\DynamoDb\DynamoDbClient([
+        'version'       => '2012-08-10',
+        'region'        => $region,
+    ]);
     $pie_dynamodb_session_handler = $dynamodb->registerSessionHandler([
-        'table_name' => isset( $_SERVER[ 'PIE_DYNAMODB_SESSION_TABLE' ] )
-            ? $_SERVER[ 'PIE_DYNAMODB_SESSION_TABLE' ]
-            : 'sessions'
+        'table_name'    => $name,
     ]);
 }
 pie_dynamodb_register_session_handler();
